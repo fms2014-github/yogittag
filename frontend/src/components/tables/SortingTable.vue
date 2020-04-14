36bb72c540d2b519e53e52f835cbdb00292bc5c1
이성준 <dhzm2aud@naver.com>
2020-04-14 15:15:03 +0900

    [Front-Component] create sorted_table component
    Referance props attributes
     - fields : field name (key)
     - items : matching field name and value
    
    S02P23D105-27 S02P23D105-29
    
    
    Former-commit-id: d5b5deb4ee68ebfe7fe93654d4312a986cc41d5f

---
+++ frontend/src/components/tables/SortingTable.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <b-table
+            :items="items"
+            :fields="fields"
+            table-variant="light"
+            head-variant="light"
+            responsive="sm"
+        ></b-table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        fields: {
+            type: Array,
+            default: function () {
+                return [
+                    {
+                        key: 'last_name',
+                        sortable: true,
+                    },
+                    {
+                        key: 'first_name',
+                        sortable: false,
+                    },
+                    {
+                        key: 'age',
+                        label: 'Person age',
+                        sortable: true,
+                    },
+                ]
+            },
+        },
+        items: {
+            type: Array,
+            default: function () {
+                return [
+                    { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+                    { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+                    { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+                    { age: 38, first_name: 'Jami', last_name: 'Carney' },
+                ]
+            },
+        },
+    },
+}
+</script>

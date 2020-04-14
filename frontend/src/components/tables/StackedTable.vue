dd587e35d55daa90cced0d951107e4c855c747c0
이성준 <dhzm2aud@naver.com>
2020-04-14 17:10:06 +0900

    [Front-Component] create stack table component
    Referance props attributes
     - Branch, Area, Tel, Address
     - store infomation
    
    S02P23D105-27
    
    
    Former-commit-id: 75c069158b6942d60bc4e5273b8221561793fcb7

---
+++ frontend/src/components/tables/StackedTable.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <b-table stacked :items="items"></b-table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            default: function () {
+                return [
+                    {
+                        Branch: null,
+                        Area: null,
+                        Tel: null,
+                        Address: null,
+                    },
+                ]
+            },
+        },
+    },
+}
+</script>

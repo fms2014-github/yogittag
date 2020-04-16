046a415690f886754523b42e82b0eb951f4d2d66
이성준 <dhzm2aud@naver.com>
2020-04-16 17:42:07 +0900

    [Front-Component] create review form component
    Register input tags
    - content
    - star rating
    - picture
    - register user ID
    
    Install vue-star-rating
    
    S02P23D105-3
    
    
    Former-commit-id: 6d8d338414e65e8c1b5b270802f5886109c33081

---
+++ frontend/src/components/forms/ReviewForm.vue
@@ -0,0 +1,120 @@
+<template>
+    <div>
+        <b-form @submit.stop.prevent>
+            <div>
+                <h5>Rating</h5>
+                <star-rating
+                    v-model="rating"
+                    :increment="0.5"
+                    :glow="6"
+                    :border-width="1"
+                    border-color="#d8d8d8"
+                    :rounded-corners="true"
+                    :star-points="[
+                        23,
+                        2,
+                        14,
+                        17,
+                        0,
+                        19,
+                        10,
+                        34,
+                        7,
+                        50,
+                        23,
+                        43,
+                        38,
+                        50,
+                        36,
+                        34,
+                        46,
+                        19,
+                        31,
+                        17,
+                    ]"
+                ></star-rating>
+            </div>
+            <br />
+            <div>
+                <h5>Content</h5>
+                <b-form-textarea
+                    id="textarea-state"
+                    v-model="content"
+                    placeholder="Enter at least 10 characters"
+                    rows="3"
+                    max-rows="6"
+                    :state="validation"
+                ></b-form-textarea>
+                <b-form-valid-feedback :state="validation"> Looks Good. </b-form-valid-feedback>
+            </div>
+            <br />
+            <div>
+                <h5>Picture</h5>
+                <b-img
+                    v-if="imageData.length > 0"
+                    center
+                    thumbnail
+                    fluid
+                    :src="imageData"
+                    alt="preview image file"
+                ></b-img>
+                <b-form-file
+                    id="preview"
+                    v-model="file"
+                    @change="previewImage"
+                    placeholder="Choose a file or drop it here..."
+                    drop-placeholder="Drop file here..."
+                ></b-form-file>
+            </div>
+            <br />
+        </b-form>
+    </div>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+    components: {
+        StarRating,
+    },
+    data() {
+        return {
+            file: null,
+            content: '',
+            rating: 0,
+            imageData: '', // we will store base64 format of image in this string
+        }
+    },
+    computed: {
+        validation() {
+            return this.content.length > 4
+        },
+    },
+    methods: {
+        previewImage: function (event) {
+            // Reference to the DOM input element
+            var input = event.target
+            // Ensure that you have a file before attempting to read it
+            if (input.files && input.files[0]) {
+                // create a new FileReader to read this image and convert to base64 format
+                var reader = new FileReader()
+                // Define a callback function to run, when FileReader finishes its job
+                reader.onload = (e) => {
+                    // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+                    // Read image as base64 and set to imageData
+                    this.imageData = e.target.result
+                }
+                // Start the reader job - read file as a data url (base64 format)
+                reader.readAsDataURL(input.files[0])
+            }
+        },
+    },
+}
+</script>
+<style scoped>
+#preview {
+    width: 700px;
+    height: 400px;
+}
+</style>

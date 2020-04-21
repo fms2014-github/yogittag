9c4feeb41964939d0404493df86126b85a19efda
이성준 <dhzm2aud@naver.com>
2020-04-22 02:04:54 +0900

    [DBModeling] update user model
    Update Object attr:
     - id(pk) auto_increment
    
    Add Object attr:
     - name
     - nick_name
     - birthday
     - address
     - profile_picture
     - cover_picture
     - followers : many to many self
    
    S02P23D105-10
    
    
    Former-commit-id: 8ffb2f60138d3a1696f21efdfaab92cf5a3dbc83

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,8 +1,16 @@
 from django.utils import timezone
 from django.db import models
+from datetime import datetime
 
 
 class User(models.Model):
-    id = models.IntegerField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, null=True)
+    nick_name = models.CharField(max_length=50, null=True)
     gender = models.CharField(max_length=50, null=True)
+    birthday = models.CharField(max_length=50, null=True)
+    address = models.TextField(null=True)
+    profile_picture = models.TextField(null=True)
+    cover_picture = models.TextField(null=True)
     born_year = models.IntegerField(null=True)
+    followers = models.ManyToManyField("self")

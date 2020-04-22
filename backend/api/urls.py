c71a2491b6295cde4f30be0a02e5fada5c2f06bf
김석용 <fms2014@naver.com>
2020-04-23 02:00:21 +0900

    urls.py 충돌 잘못 병합시킨거 복구
    Former-commit-id: 165403d1752694122a1d71e70d005f169cee7190

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from django.urls import path
 
 # 현재 페이지에서 views.py를 읽어온다.
-from . import views
+from . import views, reviewView, menuView, storeView
 
 # 주소 뒤에 book를 붙이면 getRequest 함수의 리턴값이 호출된다.
 urlpatterns = [
@@ -21,12 +21,17 @@ urlpatterns = [
     path('login', views.user_login),
     path('session-check', views.session_refresh),
     path('sign-up', views.sign_up),
-    path('user-delete', views.user_delete),
-    path('bhour-store', views.bhour_find_by_store),
-    path('menu-store', views.menu_find_by_store),
-    path('review-store', views.review_find_by_store),
-    path('review-user', views.review_find_by_user),
-    path('store-name', views.store_find_by_name),
+    path('review/<int:id>', reviewView.detail),
+    path('review/<int:score>/score', reviewView.review_list),
+    path('user/<int:user>/review', reviewView.review_list),
+    path('menu/<int:id>', menuView.detail),
+    path('menu/<str:name>', menuView.menu_list_by_name),
+    path('menu/<int:price>/price', menuView.menu_list_by_price),
+    path('store/<int:id>', storeView.detail),
+    path('store/<str:name>', storeView.store_find_by_name),
+    path('store/<int:id>/menu', storeView.menu_find_by_store),
+    path('store/<int:id>/bhour', storeView.bhour_find_by_store),
+    path('store/<int:id>/review', storeView.review_find_by_store),
     path('check', views.oauth_code_google),
     path('check2', views.oauth_code_naver),
 ]

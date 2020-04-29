870ee6864c9f682d18204f6e4574c188d4e53e16
이채은 <dlcodms123@gmail.com>
2020-04-29 23:48:45 +0900

    [Algorithm] 동행자 Back-End - 사용자포함 동행자의 점수를 평균내서 input 데이터 만듦 - SVD로 음식점 추천
    S02P23D105-97
    Actions (Type . to access issue actions)
    
    
    Former-commit-id: 1ba8b7f40cc97a71d8a0ae46977a2db0639d2b9a

--- backend/api/recommView.py
+++ backend/api/recommView.py
@@ -8,8 +8,11 @@ from .serializers import StoreSerializer
 import math
 import pandas as pd
 import numpy as np
-from django.db.models import Count
+from django.db.models import Count, Avg
 from scipy.sparse.linalg import svds
+
+from surprise import SVD, Reader
+from surprise.model_selection import cross_validate
 
 from .models import Review
 
@@ -38,7 +41,7 @@ def getStoresAround(lat, lng, area):
             math.sin(dLng / 2) * math.sin(dLng / 2)
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
         d = R * c
-        if d * 1000 <= 3000:
+        if d * 1000 <= 3000:  # 3km 이내
             filter_data.append(data)
 
     # 스토어 dataframe 만들기
@@ -64,17 +67,47 @@ def cal_rmse(user_rating, predicted_user_rating):
     return (rmse/cnt)**0.5
 
 
+def testsurprise(queryset):
+
+    dataframe = pd.DataFrame(list(queryset.values('id', 'store', 'score')))
+    R = dataframe.as_matrix()
+    reader = Reader(rating_scale=(0.5, 5.0))
+
+    # svd
+    algo = SVD()
+    algo.fit(R)
+
+    print(algo.pu, algo.qi, algo.bu, algo.bi)
+
+
 @api_view(['GET'])
 def recommand_based_user(request, id=None):
     # params
     params = request.GET
+    print(params.dict())
     lat = float(params.get('latitude'))
     lng = float(params.get('longitude'))
     area = params.get('area')
+    users = params.getlist('users')
+    users.append(id)
+
+    print(users)  # (68632, 539244, 21180, 209301)
+
     queryset = getStoresAround(lat, lng, area)  # 주변 음식점들의 리뷰들
 
-    user_review = queryset.filter(user_id=id)  # 유저가 리뷰한 것
-    print("유저의 해당지역 리뷰 개수 : {}".format(len(user_review)))
+    user_review = queryset.filter(
+        user_id__in=users)  # 유저가 리뷰한 것 전국 : 191개 마포구: 10개 ㅎ
+
+    print(f'id: {users[0]} reviews')
+    print(queryset.filter(user_id=users[0], score__gte=3).values())
+    print(f'id: {users[1]} reviews')
+    print(queryset.filter(user_id=users[1], score__gte=3).values())
+    print(f'id: {users[2]} reviews')
+    print(queryset.filter(user_id=users[2], score__gte=3).values())
+    print(f'id: {users[3]} reviews')
+    print(queryset.filter(user_id=users[3], score__gte=3).values())
+
+    print("유저(들)의 해당지역 리뷰 개수 : {}".format(len(user_review)))
 
     # 유저의 리뷰수가 0개 : popular 순으로
     # 유저의 리뷰수가 1~4개 : contents 기반 추천
@@ -105,7 +138,7 @@ def recommand_based_user(request, id=None):
         other_user = list(set(other_user))  # 중복제거
         # print(other_user)
 
-        semires = queryset.filter(user_id__in=other_user).values(
+        semires = queryset.filter(user_id__in=other_user, score_gte=3).values(
             'store_id').annotate(count=Count('store_id')).order_by('-count').values('store_id', 'count')[:20]  # 중복, 상위 20개
 
         result = stores.filter(id__in=[item.get('store_id')
@@ -115,16 +148,30 @@ def recommand_based_user(request, id=None):
 
     else:
         print('matrix factorization based recommand..')
+
+        # testsurprise(queryset)
+        # return Response({"result": None}, status.HTTP_200_OK)
+
         dataframe = pd.DataFrame(list(queryset.values()))
-        # print(dataframe.head())
+
+        if len(users) > 1:
+
+            # 새로만든 동행자포함 합체인물
+            result = queryset.filter(user_id__in=users).values('store_id').annotate(
+                score=Avg('score')).values('store_id', 'score')
+            df = pd.DataFrame(result)
+            df['user_id'] = id
+
+            dataframe = dataframe[~dataframe.user_id.isin(users)]
+            dataframe = dataframe.append(df)
 
         user_rating = pd.pivot_table(
             dataframe, index='user_id', columns='store_id', values='score').fillna(0)
         R = user_rating.as_matrix()
-
-        # print(R.shape)
+        print(f'매트릭스 R의 d {R.shape}dfd')
 
         # SVD
+        # R = (Nxk)(kxM) MF R.shape-1  SVD = U(sigma)Vt
         k = int(min(R.shape) / 25)
         U, sigma, Vt = svds(R, k=k)
         sigma = np.diag(sigma)  # 대각행렬로 바꿈
@@ -134,14 +181,14 @@ def recommand_based_user(request, id=None):
             np.dot(U, sigma), Vt)
 
         # rmse 측정
-        rmse = cal_rmse(R, svd_user_predicted_ratings)
-        print("k={} 일때 MF rmse : {}".format(k, rmse))
+        # rmse = cal_rmse(R, svd_user_predicted_ratings)
+        # print("k={} 일때 MF rmse : {}".format(k, rmse))
 
         svd_df = pd.DataFrame(svd_user_predicted_ratings, columns=list(
             user_rating.columns.values), index=list(user_rating.index.values))
 
+        # 평점이 높은 상위 15개
         svd_user = svd_df.ix[id].sort_values(ascending=False).head(15)
-        print(svd_user)
 
         result = stores.filter(id__in=list(svd_user.index.values))
         # print(type(svd_user))
@@ -151,14 +198,3 @@ def recommand_based_user(request, id=None):
         # k=100 일때 MF rmse : 2.4704298644120493
 
         return Response({"result": result.values()}, status.HTTP_200_OK)
-
-    """ dataframe = pd.DataFrame(list(queryset.values()))
-
-    # 리뷰개수가 5개 미만인 것은 필터링
-    count_df = dataframe.groupby(['store_id']).size().reset_index().rename(
-        columns={0: "count"})
-
-    count_df = count_df[count_df['count'] >= 5]
-
-    processed_data = pd.merge(
-        count_df, dataframe, left_on='store_id', right_on='store_id') """

2200f5ece119c0dd5607934e74dffeea3196dcaf
하승진 <up6760@gmail.com>
2020-04-30 20:42:52 +0900

    [Front-View] Create Page
    Add favicon, logo images, main font
    
    S02P23D105-102
    
    
    Former-commit-id: 1f46be23bf359d2ee87b3ce05db3cc514d4d0690

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,10 @@
 <template>
     <v-container class="mt-5" fill-height>
         <v-card-text class="text-center">
-            <p class="display-2 pa-5" style="font-family: h1c;">🍜 코딩도 식후경</p>
+            <img height="300px;" src="@/assets/img/main_logo.png" alt="요기딱" />
             <banner />
 
-            <v-btn
-                class="display-1 font-italic font-weight-bold"
-                large
-                style="font-family: 'Inconsolata';"
-                >Right Now!</v-btn
-            >
+            <v-btn class="display-1 font-italic font-weight-bold" large>Right Now!</v-btn>
             <food-friend />
             <slider title="나의 단골 맛집" :cardData="cardData" />
             <slider title="지인 추천 맛집" />
@@ -67,5 +62,16 @@ export default {
 @font-face {
     font-family: h1c;
     src: url('../assets/fonts/DXRMbxB-KSCpc-EUC-H.ttf');
+}
+@font-face {
+    font-family: 'Dovemayo-Medium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.0/Dovemayo-Medium.woff')
+        format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+* {
+    font-family: 'Dovemayo-Medium';
 }
 </style>

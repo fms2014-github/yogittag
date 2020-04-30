1e8794b04981c63300175838aa8130fa359c4557
김석용 <fms2014@naver.com>
2020-05-01 00:02:34 +0900

    [WebService Extension] store-endpoint update
    [ADD]
    
    [MODIFY]
    키워드, 필터 조건 둘 다 없으면 500m 이내의 모든 가게 >> 시간이 오래 걸려 지역 파라메터 추가
    키워드만 있으면 500m 이내에 키워드가 가게이름이나 카테고리에 포함된 가게 >> 완료
    키워드 필터조건에서 거리조건이 없으면 500m 이내의 키워드가 가게이름이나 카테고리에 포함된 가게 >> 완료
    키워드, 필터 조건 다 있으면 키워드와 필터 조건에 맞는 가게 >> 완료
    
    -- 수정 완료 --
    
    [DELETE]
    
    
    Former-commit-id: 8250b0a911be7aa2acae02e9f65f04867bf2c9c7

--- backend/api/storeView.py
+++ backend/api/storeView.py
@@ -1,13 +1,16 @@
+from django.db.models import Q
+
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-from django.db.models import Q
 
-from .models import Store, BHour, Menu, Review
+from .models import Store, BHour, Menu, Review, User
 
-from .serializers import StoreSerializer
+from .serializers import BhourSerializer, UserSerializer, StoreSerializer, ReviewSerializer, MenuSerializer, FollowSerializer, FavoriteListSerializer, FavoriteStoreSerializer
 import math
 
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 queryset = Store.objects.all()
 
 
@@ -31,13 +34,17 @@ def store_find_by_name(request):
         lng = float(params.get('longitude'))
         category = params.getlist('category[]')  # dict에 key값이 이렇게 들어가서 수정
         distance = params.get('distance')
-        result = Store.objects.filter(Q(store_name__contains=keyword) | Q(category__contains=keyword))
+        if keyword is not None:
+            keyword_result = queryset.filter(Q(store_name__contains=keyword) | Q(category__contains=keyword))
+        else:
+            area = params.get('area')
+            keyword_result = queryset.filter(address__contains=area)
         filter_data1 = []
         filter_data2 = []
         print(lat, lng, category, distance)
         R = 6378.137
         if distance is not None:
-            for data in result.values():
+            for data in keyword_result.values():
                 store_lat = float(data.get('latitude'))
                 store_lng = float(data.get('longitude'))
                 dLat = abs(store_lat * math.pi / 180.0 - lat * math.pi / 180.0)
@@ -47,8 +54,23 @@ def store_find_by_name(request):
                     math.sin(dLng / 2) * math.sin(dLng / 2)
                 c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
                 d = R * c
-                print(d*1000)
+                # print(d*1000)
                 if d * 1000 < int(distance):
+                    filter_data1.append(data)
+        else:
+            print('????')
+            for data in keyword_result.values():
+                store_lat = float(data.get('latitude'))
+                store_lng = float(data.get('longitude'))
+                dLat = abs(store_lat * math.pi / 180.0 - lat * math.pi / 180.0)
+                dLng = abs(store_lng * math.pi / 180.0 - lng * math.pi / 180.0)
+                a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(lat * math.pi / 180) * \
+                    math.cos(store_lat * math.pi / 180) * \
+                    math.sin(dLng / 2) * math.sin(dLng / 2)
+                c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+                d = R * c
+                # print(d*1000)
+                if d * 1000 < 500:
                     filter_data1.append(data)
         result = filter_data1
         print(len(result))
@@ -64,7 +86,7 @@ def store_find_by_name(request):
             result = filter_data2   # 요게 for문 바깥에 있어서 tap
 
         return Response({'result': result}, status=status.HTTP_200_OK)
-    return Response({"result": result.values()}, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
@@ -79,8 +101,17 @@ def menu_find_by_store(request, id=None):
     return Response({"result": result.values()}, status.HTTP_200_OK)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def review_find_by_store(request, id=None):
-    result = Review.objects.filter(store_id__exact=id)
-    print(result)
-    return Response({"result": result.values()}, status.HTTP_200_OK)
+    if request.method == 'GET':
+        riviews = Review.objects.filter(
+            store=id).select_related('user').order_by('-id')
+        return Response({"result": riviews.values()}, status.HTTP_200_OK)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ReviewSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+            # return JsonResponse({serializer.data}, status=200)
+        return JsonResponse(serializer.errors, status=400)

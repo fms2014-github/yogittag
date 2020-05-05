7a7761dfb9fddbb3fc2006ccb5fa10cc8cd02b89
김석용 <fms2014@naver.com>
2020-05-06 01:14:03 +0900

    QNA 버튼 클릭 시 서비스 준비중 alert 나타나도록 수정
    Former-commit-id: 44c87b2b223a6904ec1007da70b0e4b3c5992a89

--- frontend/src/components/FAQnQNAButton.vue
+++ frontend/src/components/FAQnQNAButton.vue
@@ -1,12 +1,22 @@
 <template>
     <div id="faq-qna" class="radius-button">
-        <button title="Help" id="faq-qna-button">
+        <button title="Help" id="faq-qna-button" @click="egg">
             <span class="material-icons">
                 help
             </span>
         </button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        egg(){
+            alert('QnA 서비스 준비중입니다.\n저희 서비스에 관심가져주셔서 감사합니다.')
+        }
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 #faq-qna {
